3ec24b148d0d9827f39fce9f62e9d8db6f361acd
David Alba <davidalba.dev@gmail.com>
2022-06-01 02:23:38 -0400

    asdf

--- components/CreatePassword.js
+++ components/CreatePassword.js
@@ -1,18 +1,23 @@
 import React,{useState} from 'react';
-import { StyleSheet,View, Image, TouchableOpacity,Text } from 'react-native';
+import { StyleSheet,View, Image, TouchableOpacity,Text, CheckBox,Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import PasswordField from './PasswordField'
+import PasswordField from './PasswordField';
+import Switch from 'react-native-switch-toggles';
 export default function CreatePassword() {
 
   const [password,setPassword]=useState('');
-  const [background,setBackground]=useState('black')
+  const [backcolor1,setBackColor]=useState('#A5AEC6');
+  const [backcolor2,setBackColor2]=useState('#A5AEC6')
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const [isSelected, setSelection] = useState(false);
+
   const testAlphbet = new RegExp("^(?=.*[a-z])(?=.*[A-Z])");
   const testLength= new RegExp("^(?=.{5,})");
 
   const verifyPassword=(data)=>{
     setPassword(data);
-    if(testAlphbet.test(password)) setBackground('white');
-    if(testLength.test(password)) setBackground('green');//use Async Await
+    if(testAlphbet.test(password)) setBackColor('#4BADE4');
+    if(testLength.test(password)) setBackColor2('#4BADE4');//use Async Await
     
   };
   
@@ -33,9 +38,32 @@ export default function CreatePassword() {
     <Text style={styles.descText} >New Password</Text>  
     <PasswordField verifyPassword={text=>verifyPassword(text)}/>
     <Text style={styles.descText} >New Password</Text>  
-    <PasswordField/>  
-    <Text style={styles.descText}>{background}</Text>
+    <PasswordField/>
+    <View style={styles.strongValidation}>
+      <View style={[styles.dot, { backgroundColor: backcolor2 }]} />
+      <Text style={[ { color: backcolor2,fontSize:14, marginLeft:10,fontFamily:'Roboto' }]}>Min 8 characters</Text>
     </View>
+    <View style={styles.strongValidation}>
+      <View style={[styles.dot, { backgroundColor: backcolor1 }]} />
+      <Text style={[ { color: backcolor1 ,fontSize:14, marginLeft:10,fontFamily:'Roboto'}]}>At least 1 capital letter</Text>
+    </View>
+    <View style={{flexDirection:'row',marginTop:50}}>
+      <Text style={{fontFamily:'Roboto',fontSize:17}}>Enable Sign In with Face ID?</Text>
+      <Switch style={{marginRight:0}}size={20} value={isEnabled} onChange={(value) => setIsEnabled(value)} activeTrackColor={"#45D058"}/>
+    </View >
+    <View style={{flexDirection:'row',marginTop:30}}>
+       <CheckBox
+          value={isSelected}
+          onValueChange={setSelection}
+          style={{marginTop:3, marginRight:10}}
+        />
+      <Text style={{fontSize:12,fontFamily:'Roboto',color:'#A5AEC6'}}>I understand that MetaMask cannot recover this password for me.<Text style={{color:'#2862F8'}}>Learn More</Text> </Text>
+    </View>
+    <View style={{flexDirection:'row',marginTop:30}}>
+      <View style={{heigh:80, width:80, s:5, backgroundColor:'#2862F8'}}></View>
+    </View>
+    
+  </View>
 
   );
 }
@@ -51,6 +79,17 @@ const styles = StyleSheet.create({
     fontSize:14,
     marginRight:10,
     textAlign:'center',
+  },
+  dot:{
+    height:10,
+    width:10,
+    borderRadius:5,
+    marginTop:5 
+  },
+  strongValidation:{
+    flexDirection:'row',
+    alignItems: 'center',
+    marginTop:15,
   },
   descText:{
     marginTop:30,
